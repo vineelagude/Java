2413499c604eebe9cb06ce3e9375bf728f3b8447
vineelagude <vineelagude.iiitn@gmail.com>
2018-10-16 12:44:42 +0530

    finds the factorial of given number

---
+++ src/Factorial.java
@@ -0,0 +1,9 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int number = 4, fact = 1;
+        for(int i = 1; i<=number ;i++){
+            fact = fact * i;
+        }
+        System.out.println(fact);
+    }
+}
