d17611000744e779214934cfaa254ce8033a2c92
vineelagude <vineelagude.iiitn@gmail.com>
2018-10-16 12:25:48 +0530

    successful run of leapyear

---
+++ src/LeapYear.java
@@ -0,0 +1,17 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2019;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("leap year");
+                } else {
+                    System.out.println("not leap year");
+                }
+            }
+        } else {
+            System.out.println("not leap year");
+        }
+    }
+}
+
